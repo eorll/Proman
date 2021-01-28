862061419b75a2d9a8d8fd50e2ac8668a761d067
Jaroslaw Kaszczak <kaszczak.jaroslaw@outlook.com>
2021-01-28 15:40:30 +0100

    Implemented renaming of board name in html.

--- static/js/events.js
+++ static/js/events.js
@@ -1,11 +1,46 @@
 function initEvents() {
-    renameBoardName();
+    initEditingBoardName();
 }
 
-function renameBoardName() {
-    $('.project-board-title').ondblclick(onDblClick);
+function initEditingBoardName() {
+    $('.project-board-title').on('dblclick', onDblClick);
 }
 
 function onDblClick(e) {
-    console.log(e.target)
+
+    let input = $('<input>');
+    input.attr('type', 'text');
+    input.addClass('input d-inline-block bg-transparent text-light border-0');
+    input.val(e.target.innerText);
+
+    $(e.target).addClass('d-none');
+    $(e.target).after(input);
+    input.focus();
+    input.select();
+
+    input.on('keypress', function (e) {
+        // If you pressed enter
+        if (e.which == 13) {
+            renameBoardNode($(e.target));
+        }
+    });
+    input.on('focusout', function (e) {
+        renameBoardNode($(e.target));
+    });
+    $(document).on('keydown', function (e) {
+        if (e.which == 27) {
+            displayTitle($(e.target));
+        }
+    });
+}
+
+function renameBoardNode(input$) {
+    let newTitle = input$.val();
+    input$.prev().text(newTitle);
+    displayTitle(input$);
+}
+
+function displayTitle(input$) {
+    input$.prev().removeClass('d-none');
+    input$.remove();
 }
