65d68f364bbad7cbeb72a25a557d31c75adb4355
UltraViolet5 <70701763+UltraViolet5@users.noreply.github.com>
2021-01-28 16:41:02 +0100

    events

---
+++ static/js/events.js
@@ -0,0 +1,46 @@
+function initEvents() {
+    initEditingBoardName();
+}
+
+function initEditingBoardName() {
+    $('#column_name').on('dblclick', onDblClick);
+}
+
+function onDblClick(e) {
+
+    let input = $('<input>');
+    input.attr('type', 'text');
+    input.addClass('input d-inline-block bg-transparent text-light border-0');
+    input.val(e.target.innerText);
+
+    $(e.target).addClass('d-none');
+    $(e.target).after(input);
+    input.focus();
+    input.select();
+
+    input.on('keypress', function (e) {
+        // If you pressed enter
+        if (e.which === 13) {
+            renameBoardNode($(e.target));
+        }
+    });
+    input.on('focusout', function (e) {
+        renameBoardNode($(e.target));
+    });
+    $(document).on('keydown', function (e) {
+        if (e.which === 27) {
+            displayTitle($(e.target));
+        }
+    });
+}
+
+function renameBoardNode(input$) {
+    let newTitle = input$.val();
+    input$.prev().text(newTitle);
+    displayTitle(input$);
+}
+
+function displayTitle(input$) {
+    input$.prev().removeClass('d-none');
+    input$.remove();
+}
