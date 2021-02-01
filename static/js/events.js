470deafa15f8404bab50624efe910d916e23437c
UltraViolet5 <70701763+UltraViolet5@users.noreply.github.com>
2021-02-01 16:48:29 +0100

    CreateCard

--- static/js/events.js
+++ static/js/events.js
@@ -1,6 +1,8 @@
 function initEvents() {
     initEditingBoardName();
     initEditingColumnName();
+    addCard();
+
 }
 
 function initEditingColumnName() {
@@ -11,12 +13,15 @@ function initEditingBoardName() {
     $('.project-board-title').on('dblclick', onDblClick);
 }
 
+
+
+
 function onDblClick(e) {
 
     let input = $('<input>');
     input.attr('type', 'text');
 
-    if(!$(e.target).hasClass('project-board-title')) {
+    if (!$(e.target).hasClass('project-board-title')) {
         input.addClass('input d-inline-block bg-transparent text-light border-0 my-0 text-center w-100');
     } else {
         input.addClass('input d-inline-block bg-transparent text-light border-0 my-0');
@@ -54,4 +59,16 @@ function renameBoardNode(input$) {
 function displayTitle(input$) {
     input$.prev().removeClass('d-none');
     input$.remove();
+}
+
+function addCard() {
+    $(".add_card_button").on("click", function (e) {
+        console.log("event on")
+        let span = $("<Span>").attr({class:"card_title d-inline me-auto"});
+        let button = $("<button>" ).attr({type:'button', class:'addedCard btn btn-primary d-block w-100 my-3', draggable:'true'});
+        span.text("New Card");
+        button.append(span);
+        $(e.target).before(button);
+        button.on('dblclick', onDblClick)
+    });
 }
